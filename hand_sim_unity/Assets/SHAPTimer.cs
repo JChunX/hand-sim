a8dd4a9297bd228cb1e23637eed0ed16a4a55937
JChunX <jasonxie@live.unc.edu>
2021-03-20 17:52:56 -0400

    Add SHAP features

---
+++ hand_sim_unity/Assets/SHAPTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHAPTimer : MonoBehaviour
+{
+    public string savepath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collisionevent");
+    }
+}
